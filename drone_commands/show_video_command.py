1ee798d72f441f814a81e98d8f67d079cbdba4bf
Kyle Corry <kylecorry31@gmail.com>
2022-04-18 15:19:36 -0400

    Run video in separate process (Python 2.7)
    Closes #3

--- drone_commands/show_video_command.py
+++ drone_commands/show_video_command.py
@@ -1,3 +1,4 @@
+import subprocess
 import threading
 
 import cv2
@@ -11,48 +12,17 @@ class ShowVideoCommand(Command):
     def __init__(self, drone: Drone):
         super().__init__(None)
         self.drone = drone
-        self.show_thread = threading.Thread(target=self.__show_video)
-        self.read_thread = threading.Thread(target=self.__read_video)
-        self.read_thread.daemon = True
-        self.stopped = False
-        self.frame = None
+        self.process = None
 
     def initialize(self):
-        self.stopped = False
         self.drone.stream()
-        self.show_thread.start()
-        self.read_thread.start()
+        # TODO: Create a virtual environment for this
+        self.process = subprocess.Popen("C:\\Users\\Kylec\\anaconda3\\envs\\tello2.7\\python.exe video/video.py",
+                                        stderr=subprocess.DEVNULL)
 
     def end(self, interrupted):
-        self.stopped = True
-        self.show_thread.join()
-        self.read_thread.join()
-
-    def __read_video(self):
-        video = self.drone.get_video()
-        video.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-        video.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
-        while not self.stopped:
-            try:
-                if video.isOpened():
-                    ret, img = video.read()
-                    if ret:
-                        self.frame = img
-            except Exception as e:
-                pass
-
-    def __show_video(self):
-        while not self.stopped:
-            try:
-                f = self.frame
-                if f is not None:
-                    # f = cv2.resize(f, (480, 320))
-                    cv2.imshow("Tello", f)
-                # time.sleep(1 / 30)
-                if cv2.waitKey(1) & 0xFF == ord('x'):
-                    break
-            except Exception as e:
-                pass
+        if interrupted and self.process is not None:
+            self.process.kill()
 
     def is_finished(self):
-        return self.stopped
+        return False
---
+++ video/video.py
@@ -0,0 +1,56 @@
+import socket
+from threading import Thread
+import libh264decoder
+import numpy as np
+import cv2
+
+decoder = libh264decoder.H264Decoder()
+socket_video = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_video.bind(('0.0.0.0', 11111))
+frame = None
+
+
+def _receive_video_thread():
+    global frame
+    packet_data = ""
+    while True:
+        try:
+            res_string, ip = socket_video.recvfrom(2048)
+            packet_data += res_string
+            # end of frame
+            if len(res_string) != 1460:
+                for f in _h264_decode(packet_data):
+                    frame = cv2.cvtColor(f, cv2.COLOR_RGB2BGR)
+                packet_data = ""
+
+        except socket.error as exc:
+            print("Caught exception socket.error : %s" % exc)
+
+
+def _h264_decode(packet_data):
+    res_frame_list = []
+    frames = decoder.decode(packet_data)
+    for framedata in frames:
+        (f, w, h, ls) = framedata
+        if f is not None:
+            f = np.fromstring(f, dtype=np.ubyte, count=len(f), sep='')
+            f = (f.reshape((h, ls / 3, 3)))
+            f = f[:, :w, :]
+            res_frame_list.append(f)
+
+    return res_frame_list
+
+
+receive_video_thread = Thread(target=_receive_video_thread)
+receive_video_thread.daemon = True
+receive_video_thread.start()
+
+while True:
+    try:
+        f = frame
+        if f is not None:
+            cv2.imshow("Tello", f)
+        if cv2.waitKey(1) & 0xFF == ord('x'):
+            break
+    except Exception as e:
+        pass

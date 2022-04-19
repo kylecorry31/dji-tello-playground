e29b8a1410867f2466b716b68b76572f4eead41a
Kyle Corry <kylecorry31@gmail.com>
2022-04-19 19:43:56 -0400

    Fix rename

--- drone/advanced/tello2.py
+++ drone/advanced/tello2.py
@@ -14,60 +14,60 @@ class Tello2:
         self.state_conn.listen(self.__state_listener)
 
     def command(self):
-        ret = self.command_conn.send(api.command(), wait=True)
-        if ret is None or ret is api.error():
+        ret = self.command_conn.send(sdk.command(), wait=True)
+        if ret is None or ret is sdk.error():
             raise Exception("Could not connect to Tello")
 
     def takeoff(self):
-        self.command_conn.send(api.takeoff())
+        self.command_conn.send(sdk.takeoff())
 
     def land(self):
-        self.command_conn.send(api.land())
+        self.command_conn.send(sdk.land())
 
     def rc(self, x: float, y: float, z: float, yaw: float):
-        self.command_conn.send(api.rc(self.__percent(x), self.__percent(y), self.__percent(z),
+        self.command_conn.send(sdk.rc(self.__percent(x), self.__percent(y), self.__percent(z),
                                       self.__percent(yaw)))
 
     def set_stream(self, is_on: bool):
         if is_on:
-            self.command_conn.send(api.streamon())
+            self.command_conn.send(sdk.streamon())
         else:
-            self.command_conn.send(api.streamoff())
+            self.command_conn.send(sdk.streamoff())
 
     def emergency(self):
-        self.command_conn.send(api.emergency())
+        self.command_conn.send(sdk.emergency())
 
     def move_x(self, x: int):
         if x > 0:
-            self.command_conn.send(api.left(x))
+            self.command_conn.send(sdk.left(x))
         else:
-            self.command_conn.send(api.right(-x))
+            self.command_conn.send(sdk.right(-x))
 
     def move_y(self, y: int):
         if y > 0:
-            self.command_conn.send(api.forward(y))
+            self.command_conn.send(sdk.forward(y))
         else:
-            self.command_conn.send(api.back(-y))
+            self.command_conn.send(sdk.back(-y))
 
     def move_z(self, z: int):
         if z > 0:
-            self.command_conn.send(api.up(z))
+            self.command_conn.send(sdk.up(z))
         else:
-            self.command_conn.send(api.down(-z))
+            self.command_conn.send(sdk.down(-z))
 
     def rotate(self, yaw: int):
         if yaw > 0:
-            self.command_conn.send(api.cw(yaw))
+            self.command_conn.send(sdk.cw(yaw))
         else:
-            self.command_conn.send(api.ccw(-yaw))
+            self.command_conn.send(sdk.ccw(-yaw))
 
     def go(self, x: int, y: int, z: int, speed: float):
         s = self.__percent(speed)
-        self.command_conn.send(api.go(x, y, z, s))
+        self.command_conn.send(sdk.go(x, y, z, s))
 
     def curve(self, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int, speed: float):
         s = self.__percent(speed)
-        self.command_conn.send(api.curve(x1, y1, z1, x2, y2, z2, s))
+        self.command_conn.send(sdk.curve(x1, y1, z1, x2, y2, z2, s))
 
     def get_state(self):
         return self.state

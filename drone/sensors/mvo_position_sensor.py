6b25289bc9d43b3e02fc58c9c5cee907493fbcae
Kyle Corry <kylecorry31@gmail.com>
2022-04-19 17:30:31 -0400

    Fix x and y speed

--- drone/sensors/mvo_position_sensor.py
+++ drone/sensors/mvo_position_sensor.py
@@ -19,7 +19,7 @@ class MVOPositionSensor(Sensor):
         speed = self.mvo.read()
         if speed is None:
             return
-        (speed_x, speed_y) = rotate((speed[0], speed[1]), self.compass.read())
+        (speed_x, speed_y) = rotate((speed[1], speed[2]), self.compass.read())
         self.pos = (
             self.pos[0] + speed_x * dt,
             self.pos[1] + speed_y * dt,

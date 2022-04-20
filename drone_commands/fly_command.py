230e7ee03d38057ba42a19d0fff361bf145473ac
Kyle Corry <kylecorry31@gmail.com>
2022-04-20 19:16:58 -0400

    Auto set maintain height

--- drone_commands/fly_command.py
+++ drone_commands/fly_command.py
@@ -25,13 +25,8 @@ class FlyCommand(Command):
         self.last_time = time.time()
 
     def execute(self):
-        self.handle_field_orientation()
-        z_adjust = self.handle_altitude_maintain()
-        z = (self.controller.get_trigger(RT) - self.controller.get_trigger(LT)) * self.z_speed
-        if abs(z) < 0.1:
-            z = z_adjust
-        else:
-            self.maintain_height = None
+        self.update_field_orientation()
+        z = self.get_z()
         x = self.controller.get_x(RIGHT_STICK) * self.x_speed
         y = self.controller.get_y(RIGHT_STICK) * self.y_speed
         yaw = self.controller.get_x(LEFT_STICK) * self.yaw_speed
@@ -39,18 +34,24 @@ class FlyCommand(Command):
         self.last_time = time.time()
         self.drone.fly(x, y, z, yaw, self.is_field_oriented, fast_mode)
 
-    def handle_altitude_maintain(self):
+    def get_z(self):
         dt = time.time() - self.last_time
-        if self.controller.get_button(X):
-            self.maintain_height = self.drone.altimeter.read()
-            print("Maintain height:", self.maintain_height)
+        user = (self.controller.get_trigger(RT) - self.controller.get_trigger(LT)) * self.z_speed
 
-        if self.maintain_height is None:
-            return 0
+        # If the user is not moving, maintain the height
+        if abs(user) < 0.1 and self.maintain_height is not None:
+            return self.maintain_pid.calculate(self.drone.altimeter.read(), self.maintain_height, dt)
 
-        return self.maintain_pid.calculate(self.drone.altimeter.read(), self.maintain_height, dt)
+        # If the user is moving up/down, set the new maintain height
+        self.maintain_height = self.drone.altimeter.read()
 
-    def handle_field_orientation(self):
+        # Don't maintain height if it is too high or low
+        if self.maintain_height <= 10 or self.maintain_height > 1000:
+            self.maintain_height = None
+
+        return user
+
+    def update_field_orientation(self):
         thumb = self.controller.get_button(LEFT_THUMB)
         if thumb != self.last_thumb and thumb:
             self.is_field_oriented = not self.is_field_oriented
